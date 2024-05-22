03b01a9a6da5f1c704f3a01f25fad175bb769618
elvisrexhaa <elvis2000@hotmail.co.uk>
2024-05-22 15:04:28 +0100

    Home View + Article Mock Data for REST API placeholder

---
+++ LaskNewsMobileApp/Core/Models/Article.swift
@@ -0,0 +1,8 @@
+//
+//  Article.swift
+//  LaskNewsMobileApp
+//
+//  Created by Elvis Rexha on 22/05/2024.
+//
+
+import Foundation
---
+++ LaskNewsMobileApp/Extensions/View Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  View Extensions.swift
+//  LaskNewsMobileApp
+//
+//  Created by Elvis Rexha on 22/05/2024.
+//
+
+import SwiftUI
+
+struct View_Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_Extensions()
+}

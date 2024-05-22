82fcf5e6ceabd57b6a631ada0376fcca2d65b514
elvisrexhaa <elvis2000@hotmail.co.uk>
2024-05-22 13:28:30 +0100

    Initial Commit

---
+++ LaskNewsMobileApp/LaskNewsMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaskNewsMobileAppApp.swift
+//  LaskNewsMobileApp
+//
+//  Created by Elvis Rexha on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LaskNewsMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
